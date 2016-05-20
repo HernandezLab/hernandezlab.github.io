841525541255472fc7a8e9d2ac58e00bbd22a99b
Kevin Hartman <kevin.hartman@ucsf.edu>
2016-05-20 14:46:39 -0700

    Publications automation

--- publications/citations/nbib_to_yml.py
+++ publications/citations/nbib_to_yml.py
@@ -5,6 +5,7 @@ import sys
 import yaml
 import operator
 import datetime
+from titlecase import titlecase
 
 include_fields = ['AB', 'AID', 'AU', 'DEP', 'DP', 'FAU', 'JT', 'LID', 'PMID', 'TA', 'TI']
 # include_fields = ['DEP', 'DP', 'TI']
@@ -26,7 +27,19 @@ with open(sys.argv[1],'r') as f:
           pubs[pmid][field].append(contents)
         else:
           pubs[pmid][field] = [contents]
-      
+        
+        # Add consortia to authors
+        if field == 'CN':
+          if 'AU' in pubs[pmid]:
+            pubs[pmid]['AU'].append(contents)
+          else:
+            pubs[pmid]['AU'] = [contents]
+          if 'FAU' in pubs[pmid]:
+            pubs[pmid]['FAU'].append(contents)
+          else:
+            pubs[pmid]['FAU'] = [contents]
+          
+      # Process line
       field = line[0:4].strip()
       contents = line[6:].strip()
       
@@ -50,12 +63,14 @@ for pmid, pub in pubs.items():
   pub_reformat = {}
   
   pub_reformat['title'] = pub['TI'][0]
+  
   pub_reformat['authors'] = pub['AU']
-  pub_reformat['authors full'] = pub['FAU']
-  pub_reformat['abstract'] = pub['AB']
-  pub_reformat['journal'] = pub['TA']
-  pub_reformat['journal full'] = pub['JT']
-  pub_reformat['pmid'] = pub['PMID']
+  pub_reformat['authors_full'] = pub['FAU']
+  
+  pub_reformat['abstract'] = pub['AB'][0]
+  pub_reformat['journal'] = pub['TA'][0]
+  pub_reformat['journal_full'] = titlecase(pub['JT'][0])
+  pub_reformat['pmid'] = pub['PMID'][0]
   
   try:
     date = datetime.datetime.strptime(pub['DP'][0], '%Y %b %d')
@@ -66,6 +81,15 @@ for pmid, pub in pubs.items():
       date = datetime.datetime.strptime(pub['DEP'][0], '%Y%m%d')
   pub_reformat['date'] = date
   pub_reformat['year'] = date.year
+  
+  ids = []
+  if 'AID' in pub:
+    ids = ids+pub['AID']
+  if 'LID' in pub:
+    ids = ids+pub['LID']
+  doi_ids = [x for x in ids if ' [doi]' in x]
+  doi = doi_ids[0].replace(' [doi]','')
+  pub_reformat['doi'] = doi
   
 #   AID LID
   
